df97ddd0f24075d3fcaf300bff000a5480584829
Wiktor Mociun <voter101@gmail.com>
2015-07-08 21:13:06 +0200

    Skeleton Index file with template for whole app

--- index.ios.js
+++ index.ios.js
@@ -12,31 +12,92 @@ var {
   View,
 } = React;
 
-var PeopleCountPicker = require('./PeopleCountPicker');
-var FairPizza = React.createClass({
-  render: function() {
+var Pizza              = require('./Pizza');
+var CuttingInstruction = require('./CuttingInstruction');
+var PeopleCountPicker  = require('./PeopleCountPicker');
+
+class FairPizza extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { peopleCount: 4 };
+    this.setPeopleCount = this.setPeopleCount.bind(this);
+    this.cuttingEdges = this.cuttingEdges.bind(this);
+  }
+
+  setPeopleCount(peopleCount) {
+    this.setState({ peopleCount: peopleCount });
+  }
+
+  cuttingEdges() {
+    return this.state.peopleCount * 2;
+  }
+
+  render() {
     return (
-      <View style={styles.container}>
-        <PeopleCountPicker />
-        <Text style={styles.welcome}>
-          Welcome to React Native!!!
-        </Text>
+      <View style={styles.app}>
+        {this.pizza()}
+        {this.cuttingInstruction()}
+        {this.peoplePicker()}
       </View>
     );
   }
-});
+
+  pizza() {
+    return (
+      <View style={styles.pizza}>
+        <Pizza cuttingEdges={this.cuttingEdges()} />
+      </View>
+    );
+  }
+
+  cuttingInstruction() {
+    return (
+      <View style={styles.cuttingInstruction}>
+        <CuttingInstruction
+          cuttingEdges={this.cuttingEdges()}
+          peopleCount={this.state.peopleCount}
+        />
+      </View>
+    );
+  }
+
+  peoplePicker() {
+    return (
+      <View style={styles.peoplePickerWrapper}>
+        <View style={styles.peoplePicker}>
+          <PeopleCountPicker
+            peopleCount={this.state.peopleCount}
+            setPeopleCount={this.setPeopleCount}
+          />
+        </View>
+      </View>
+    );
+  }
+}
 
 var styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
     backgroundColor: '#C41D47',
   },
-  welcome: {
-    fontSize: 20,
-    marginTop: 40,
-    textAlign: 'center',
-    margin: 5,
+  pizza: {
+    alignSelf: 'center',
+    marginTop: 40
   },
+  cuttingInstruction: {
+    alignSelf: 'center',
+    padding: 20,
+    paddingTop: 0
+  },
+  peoplePickerWrapper: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  peoplePicker: {
+    height: 95,
+    overflow: 'hidden',
+    justifyContent: 'flex-end',
+  }
 });
 
 AppRegistry.registerComponent('FairPizza', () => FairPizza);
